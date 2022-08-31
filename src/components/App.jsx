d88a42f849f6543e58e2ac045bee143f6af63ee3
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-08-31 19:33:14 +0300

    add  app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import { HeaderNav } from './HeaderNav/HederNav';
 import { Hero } from './Hero/Hero';
 import { Users } from './Users/Users';
 import { SingUp } from './SingUp/SingUp';
+import Modal from './Modal/Modal';
 
 export const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div>
-      <HeaderNav />
-      <Hero />
+      <HeaderNav toggleModal={toggleModal} />
+      <Hero toggleModal={toggleModal} />
       <Users />
-      <SingUp />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <SingUp showModal={toggleModal} />
+        </Modal>
+      )}
     </div>
   );
 };
