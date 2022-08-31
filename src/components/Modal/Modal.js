12d1363676197ad1348014011ce9cf94f8092ea3
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-08-31 19:33:39 +0300

    modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+  const handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
+Modal.propTypes = {
+  children: PropTypes.node,
+  onClose: PropTypes.func.isRequired,
+};
+export default Modal;
